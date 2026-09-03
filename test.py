11e69f22c81d78e49a2ec5846ee628c101e1148b
Muhammad Bilal <iambilalmirza96@gmail.com>
2026-09-03 15:34:44 +0200

    Add automated test workflow

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-print("Hell" \
-"" \
-"")
+from playwright.sync_api import sync_playwright, expect
+
+def test_open_boozt():
+    with sync_playwright() as p:
+        # Use Firefox instead of Chromium
+        browser = p.firefox.launch(headless=False)
+        page = browser.new_page()
+        
+        page.goto("https://www.boozt.com/")
+        
+        expect(page).to_have_url("https://www.boozt.com/")
+        
+        browser.close()
+
+if __name__ == "__main__":
+    test_open_boozt()
